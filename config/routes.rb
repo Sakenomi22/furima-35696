ba5644fc136598cc30175689309100f4e39ae702
Sakenomi22 <shiriya19@outlook.jp>
2021-06-01 12:29:41 +0900

    Revert "デバイスの導入やり直し"
    This reverts commit 8a6d8757426dc3f02025de6ab88cea42d6b58c36.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users
-  root to: "products#index"
-=======
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
->>>>>>> parent of ebdeb8b (deviseの導入)
 end
