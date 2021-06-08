41873a13d3155a96bed7b880181a82773af8a6fc
Sakenomi22 <shiriya19@outlook.jp>
2021-06-08 14:27:10 +0900

    before_actionの使用

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_product, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @products = Product.all.order(created_at: :desc)
@@ -35,13 +35,8 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    product = Product.find(params[:id])
-    if product.user.id == current_user.id
-      product.destroy
+      @product.destroy
       redirect_to root_path
-    else
-      redirect_to root_path
-    end
   end
 
   private
